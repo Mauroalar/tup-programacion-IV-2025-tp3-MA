a18bf00d375362c9af61edc8e8b768e7fd30bbdc
Alarcon Mauro <maurohugoalarcon@gmail.com>
2025-11-13 01:09:24 -0300

    Se crea en el Front-end, el componente Viajes.jsx

---
+++ front-end/src/features/Viajes/Componentes/Viajes.jsx
@@ -0,0 +1,11 @@
+import { useState, useEffect } from "react";
+import { useAuth } from "../../../context/Auth";
+
+
+export const Viaje = () => {
+    return(
+        <>
+            <h1>Listado de Viajes</h1>        
+        </>
+    )
+}
