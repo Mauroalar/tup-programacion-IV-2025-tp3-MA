0c6a3937e44d60959abeb8f3618c786f2a64eceb
Alarcon Mauro <maurohugoalarcon@gmail.com>
2025-11-13 01:04:49 -0300

    Se crea el archivo Layout.jsx para las rutas de las paginas. dentro de la carpeta features en Front-end

---
+++ front-end/src/features/Layout/Layout.jsx
@@ -0,0 +1,39 @@
+import { Outlet, Link } from "react-router";
+import { useAuth } from "../../context/Auth";
+import { Ingresar } from "../Login/Componente/Ingresar";
+import { AuthElements } from "../../context/Auth";
+
+export const Layout = () => {
+  const { isAuthenticated, logout } = useAuth();
+
+  return (
+    <main className="container">
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <AuthElements>
+            <li>
+                <Link to="/vehiculos">Vehiculos</Link>
+            </li>
+            <li>
+                <Link to="/conductores">Conductores</Link>
+            </li>
+            <li>
+                <Link to="/viajes">viajes</Link>
+            </li>
+          </AuthElements>
+        </ul>
+        <li>
+          {isAuthenticated ? (
+            <button onClick={() => logout()}>Salir</button>
+          ) : (
+            <Ingresar />
+          )}
+        </li>
+      </nav>
+      <Outlet />
+    </main>
+  );
+};
