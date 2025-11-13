ea754565e185d5461eb8f0a9f13847bd6feba2b5
Alarcon Mauro <maurohugoalarcon@gmail.com>
2025-11-13 00:52:51 -0300

    En el front-end se modifican archivos App y main jsx

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,35 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+
+export function App() {
+  const [session, setSession] = useState(null);
+  const [nombre, setNombre] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const response = await fetch("http://localhost:3000/auth/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ nombre, password }),
+    });
+
+    const session = await response.json();
+
+    if (!response.ok && response.status === 400) {
+      console.log("Error:", session.error);
+      return;
+    }
+
+    setSession(session);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>React+login</h1>
+      {!session && (
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="nombre">Usuario:</label>
+          <input
+            name="nombre"
+            value={nombre}
+            onChange={(e) => setNombre(e.target.value)}
+          />
+          <label htmlFor="password">Contraseña:</label>
+          <input
+            name="password"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button type="submit">Ingresar</button>
+        </form>
+      )}
+      {session && (
+        <>
+          <button onClick={() => setSession(null)}>Salir</button>
+          <h2>Contador</h2>
+          <Contador />
+          <h2>Usuarios</h2>
+          <Usuarios token={session.token} />
+        </>
+      )}
     </>
-  )
+  );
 }
-
-export default App
--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -1,10 +1,34 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+//import "./index.css";
+import "@picocss/pico"
+import { App } from "./App.jsx";
+import { AuthProvider } from "./context/Auth.jsx";
+import { Route, Routes, BrowserRouter } from "react-router";
+import { Layout } from "./features/Layout/Layout.jsx";
+import { Vehiculo } from "./features/vehiculos/componentes/Vehiculo.jsx";
+import { Conductores } from "./features/Conductores/Componentes/Conductores.jsx";
+import { Viaje } from "./features/Viajes/Componentes/Viajes.jsx";
+import { AuthPage } from "./context/Auth.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+  <AuthProvider>
+    <StrictMode>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout/>}>
+            <Route index element={<App/>}/>
+            <Route path="/vehiculos" element={
+                <AuthPage>
+                  <Vehiculo/>
+                </AuthPage>
+            }/>
+            <Route path="/conductores" element={<Conductores/>}/>
+            <Route path="/viajes" element={<Viaje/>}/>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </StrictMode>
+  </AuthProvider>
+  
+);
