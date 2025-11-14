3db4dfc61710a758bd3ff58c2d670f2f7dfaaa70
Alarcon Mauro <maurohugoalarcon@gmail.com>
2025-11-14 01:56:27 -0300

    Se agrega en el Front-end en el archivo main.jsx las nuevas rutas.

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -7,7 +7,9 @@ import { AuthProvider } from "./context/Auth.jsx";
 import { Route, Routes, BrowserRouter } from "react-router";
 import { Layout } from "./features/Layout/Layout.jsx";
 import { Vehiculo } from "./features/vehiculos/componentes/Vehiculo.jsx";
+import { ModificarVehiculo } from "./features/vehiculos/componentes/VehiculoMod.jsx";
 import { Conductor } from "./features/Conductores/Componentes/Conductores.jsx";
+import { CreateConductor } from "./features/Conductores/Componentes/ConductoresCread.jsx";
 import { ModificarConductor } from "./features/Conductores/Componentes/ConductoresMod.jsx";
 import { Viaje } from "./features/Viajes/Componentes/Viajes.jsx";
 import { AuthPage } from "./context/Auth.jsx";
@@ -25,7 +27,9 @@ createRoot(document.getElementById("root")).render(
                 </AuthPage>
             }/>
             <Route path="/conductores" element={<Conductor/>}/>
+            <Route path="/conductores/crear" element={<CreateConductor/>}/>
             <Route path="/conductores/:id/modificar" element={<ModificarConductor/>}/>
+            <Route path="/vehiculos/:id/modificar" element={<ModificarVehiculo/>}/>
             <Route path="/viajes" element={<Viaje/>}/>
           </Route>
         </Routes>
